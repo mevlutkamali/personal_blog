e163aa024cf193d2548dfeb09273a75316606223
Mevlüt KAMALI <116831651+mevlutkamali@users.noreply.github.com>
2025-05-04 00:13:33 +0300

    Add files via upload

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById('menu-ikons');
-const navLinks = document.querySelector('.nav-links');
-
-menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
