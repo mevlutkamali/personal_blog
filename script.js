fecb202dbdc151d9bd23128259137de15397a3c7
Mevlüt KAMALI <116831651+mevlutkamali@users.noreply.github.com>
2025-05-25 16:51:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
+// Tema butonu
 const themeToggle = document.getElementById('themeToggle');
 
-// Daha önce kaydedilmiş tema varsa onu uygula
 if (localStorage.getItem('theme') === 'dark') {
     document.body.classList.add('dark-mode');
 }
 
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-
-    // Tema tercihini kaydet
-    if (document.body.classList.contains('dark-mode')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
+    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
 });
